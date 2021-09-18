a63a7e6c76ef1f50d5ab6661741e802e9d4ec9ae
ziyongchen <14032635@studnet.uts.edu.au>
2021-09-18 22:32:25 +1000

    update 1.6

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -8,13 +8,15 @@ public class LevelGenerator : MonoBehaviour
     private GameObject Wall;
     public float positionX;
     public float positionY;
+    private int wallsid = 0;
     // Start is called before the first frame update
+    //Data of four maps (initial x-axis, initial y-axis, x-axis creation direction, y-axis creation direction, map id)
     void Start()
     {
-        CreateMap(-4, -6, 1, 1, 1);
-        CreateMap(-23, -6, -1, 1, 2);
-        CreateMap(-4, -22, 1, -1, 3);
-        CreateMap(-23, -22, -1, -1, 4);
+        CreateMap(-4, -6, 1, 1, 0);
+        CreateMap(-23, -6, -1, 1, 1);
+        CreateMap(-4, -22, 1, -1, 2);
+        CreateMap(-23, -22, -1, -1, 3);
     }
 
     // Update is called once per frame
@@ -25,6 +27,7 @@ public class LevelGenerator : MonoBehaviour
 
     private void CreateMap(float originalX, float originalY, int Xdirection, int Ydirection, int id)
     {
+        
         int[,] levelMap =
         {
             {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
@@ -43,58 +46,38 @@ public class LevelGenerator : MonoBehaviour
             {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
             {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
         };
-
+//By traversing the horizontal and vertical of the icon
         for (int y = 0; y < levelMap.GetLength(0); y++){
         for (int x = 0; x < levelMap.GetLength(1); x++){
+            //Through the horizontal and vertical (x, y) data in the levelmap, put down the corresponding walls picture
                 switch (levelMap[y, x]){ 
                 case 1:
-                    Wall = (GameObject)Instantiate(transArray[0], transform);
-                    positionX = (originalX + x)  * Xdirection;
-                    positionY = (originalY + y)  * -Ydirection;
-                    Wall.transform.position = new Vector3(positionX, positionY,0);
-                    rotate(Wall, y, x, id);
+                wallsid=levelMap[y, x]-1;
+                    Movemap(wallsid, x, y, originalX,originalY,Xdirection,Ydirection,id);
                 break;
                 case 2:
-                    Wall = (GameObject)Instantiate(transArray[1], transform);
-                    positionX = (originalX + x)  * Xdirection;
-                    positionY = (originalY + y)  * -Ydirection;
-                    Wall.transform.position = new Vector3(positionX, positionY,0);
-                    rotate(Wall, y, x, id);
+                wallsid=levelMap[y, x]-1;
+                     Movemap(wallsid, x, y, originalX,originalY,Xdirection,Ydirection,id);
                 break;
                 case 3:
-                    Wall = (GameObject)Instantiate(transArray[2], transform);
-                    positionX = (originalX + x)  * Xdirection;
-                    positionY = (originalY + y)  * -Ydirection;
-                    Wall.transform.position = new Vector3(positionX, positionY,0);
-                    rotate(Wall, y, x, id);
+                wallsid=levelMap[y, x]-1;
+                     Movemap(wallsid, x, y, originalX,originalY,Xdirection,Ydirection,id);
                 break;
                 case 4:
-                    Wall = (GameObject)Instantiate(transArray[3], transform);
-                    positionX = (originalX + x)  * Xdirection;
-                    positionY = (originalY + y)  * -Ydirection;
-                    Wall.transform.position = new Vector3(positionX, positionY,0);
-                    rotate(Wall, y, x, id);
+                wallsid=levelMap[y, x]-1;
+                     Movemap(wallsid, x, y, originalX,originalY,Xdirection,Ydirection,id);
                 break;
                 case 5:
-                    Wall = (GameObject)Instantiate(transArray[4], transform);
-                    positionX = (originalX + x)  * Xdirection;
-                    positionY = (originalY + y)  * -Ydirection;
-                    Wall.transform.position = new Vector3(positionX, positionY,0);
-                    rotate(Wall, y, x, id);
+                wallsid=levelMap[y, x]-1;
+                     Movemap(wallsid, x, y, originalX,originalY,Xdirection,Ydirection,id);
                 break;
                 case 6:
-                    Wall = (GameObject)Instantiate(transArray[5], transform);
-                    positionX = (originalX + x)  * Xdirection;
-                    positionY = (originalY + y)  * -Ydirection;
-                    Wall.transform.position = new Vector3(positionX, positionY,0);
-                    rotate(Wall, y, x, id);
+                wallsid=levelMap[y, x]-1;
+                     Movemap(wallsid, x, y, originalX,originalY,Xdirection,Ydirection,id);
                 break;
                 case 7:
-                    Wall = (GameObject)Instantiate(transArray[6], transform);
-                    positionX = (originalX + x)  * Xdirection;
-                    positionY = (originalY + y)  * -Ydirection;
-                    Wall.transform.position = new Vector3(positionX, positionY,0);
-                    rotate(Wall, y, x, id);
+                wallsid=levelMap[y, x]-1;
+                     Movemap(wallsid, x, y, originalX,originalY,Xdirection,Ydirection,id);
                 break;
                 default:
                 Debug.Log("Empty");
@@ -102,9 +85,18 @@ public class LevelGenerator : MonoBehaviour
             }
         }
         }
-    
-        
     }
+    //Initial position (originx, originy) + levelmap (very vertical position x, y), multiplied by the icon creation direction (left to right = 1, right to left = -1, first determine whether the size of the wall is 1)
+    //Pass value to rotate
+    private void Movemap(int wallsid,int x,int y,float originalX,float originalY,int Xdirection,int Ydirection,int id){
+         Wall = (GameObject)Instantiate(transArray[wallsid], transform);
+                    positionX = (originalX + x)  * Xdirection;
+                    positionY = (originalY + y)  * -Ydirection;
+                    Wall.transform.position = new Vector3(positionX, positionY,0);
+                    rotate(Wall, y, x, id);
+    }
+
+//Make rotateMap through levelmap
 
     private void rotate(GameObject Wall, int x, int y, int id){
          int[,] rotateMap =
@@ -112,7 +104,7 @@ public class LevelGenerator : MonoBehaviour
         {0,3,3,3,3,3,3,3,3,3,3,3,3,3},
         {0,5,5,5,5,5,5,5,5,5,5,5,5,0},
         {0,5,0,3,3,3,5,0,3,3,3,3,5,0},
-        {0,6,0,0,0,2,5,0,0,0,0,2,5,0},
+        {0,5,0,0,0,2,5,0,0,0,0,2,5,0},
         {0,5,1,1,1,2,5,1,1,1,1,2,5,1},
         {0,5,5,5,5,5,5,5,5,5,5,5,5,5},
         {0,5,0,3,3,3,5,0,3,5,0,3,3,3},
@@ -126,8 +118,10 @@ public class LevelGenerator : MonoBehaviour
         {0,0,0,0,0,0,5,0,0,0,0,0,0,0},
         };
 
+//Confirm by id the figure is the upper left corner (1), upper right corner (2), lower left corner (3), lower right corner (4)
+//Confirm the rotation angle through RotateMap
     switch (id){
-        case 1:
+        case 0:
         switch (rotateMap[x, y])
         {
             case 0:
@@ -147,7 +141,7 @@ public class LevelGenerator : MonoBehaviour
         }
         break;
 
-         case 2:
+         case 1:
         switch (rotateMap[x, y])
         {
             case 0:
@@ -167,7 +161,7 @@ public class LevelGenerator : MonoBehaviour
         }
         break;
 
-        case 3:
+        case 2:
         switch (rotateMap[x, y])
         {
              case 0:
@@ -185,15 +179,12 @@ public class LevelGenerator : MonoBehaviour
               case 5:
              Wall.transform.Rotate(180f, 180f,180f);
              break;
-             case 6:
-            Wall.transform.Rotate(180f, 180f, 180f);
-             break;
             default:
             break;
         }
         break;
 
-        case 4:
+        case 3:
         switch (rotateMap[x, y])
         {
              case 0:
@@ -211,9 +202,6 @@ public class LevelGenerator : MonoBehaviour
              case 5:
             Wall.transform.Rotate(180f, 180f, 180f);
              break; 
-             case 6:
-            Wall.transform.Rotate(180f, 180f, 180f);
-             break;
             default:
             break;
         }
